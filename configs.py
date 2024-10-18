10ba06e330465f50e0667b64f703447df927b1ae
mrpanda2 <137524733+mrpanda2@users.noreply.github.com>
2024-10-18 23:41:31 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "7314923110"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dqdarkfilter2:dqdarkfilter2@dqdarkfilter2.r7w2g.mongodb.net/?retryWrites=true&w=majority&appName=dqdarkfilter2")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002162032884")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002358196072"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
